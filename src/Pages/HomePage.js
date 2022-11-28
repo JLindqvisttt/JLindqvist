5b5004b552bd5e6c079970f95b42a455889e628d
JLindqvistt <linkanjontes@gmail.com>
2022-11-28 21:29:45 +0100

    YEah

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,29 +1,31 @@
 import React from 'react';
-
-
-
-
+import "./style.css"
+import { MDBIcon} from 'mdbreact';
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 const HomePage = () => {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry)
-            if (entry.isIntersecting){
-                entry.target.classList.add('show');
-            }else entry.target.classList.remove('show');
-        });
-    });
-    const hiddenElememnts = document.querySelectorAll('.hidden');
-    hiddenElememnts.forEach((el) => observer.observe(el));
     return (
         <div>
-            <section style={{justifyContent: 'center', alignItems: 'center', heigth: '90vh'}}>
+            <section className="show bg-section text-white bg-dark">
                 <div className="bg-image ">
+                    <h1>Hello there, my name is Jonathan Lindqvist</h1>
+                    <MDBIcon icon='camera-retro' />
+                    <h3 className="text-dark">Go down and see all the projects</h3>
                 </div>
             </section>
-            <section className="hidden bg-section text-white">
-                <h2>Projects</h2>
-                <p>KTH Ergonomi</p>
+
+            <section className="show bg-section text-white bg-dark">
+                <div className="sectionCenter">
+
+                    <h1 className="fw-bold">
+                        Projects that I have built
+                        <MDBIcon icon="lightbulb " size="2px" className=""/>
+                    </h1>
+
+                    <h2>Projects</h2>
+                    <p>KTH Ergonomi</p>
+                </div>
+
             </section>
             <section className="hidden">
                 <h2>Projects</h2>
