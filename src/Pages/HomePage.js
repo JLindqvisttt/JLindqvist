b594aeacf5c86dece24711f05227fe3b40f7c1e2
JLindqvistt <linkanjontes@gmail.com>
2022-11-28 22:08:06 +0100

    aaa

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,33 +1,63 @@
 import React from 'react';
 import "./style.css"
-import { MDBIcon} from 'mdbreact';
+import {MDBIcon} from 'mdbreact';
 import "@fortawesome/fontawesome-free/css/all.min.css"
-
+import Typical from 'react-typical'
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 const HomePage = () => {
     return (
         <div>
             <section className="show bg-section text-white bg-dark">
                 <div className="bg-image ">
-                    <h1>Hello there, my name is Jonathan Lindqvist</h1>
-                    <MDBIcon icon='camera-retro' />
+                    <div className="container ">
+                        <div className="row justify-content-md-center">
+                            <div className="col">
+                                <h1 className="text-center" style={{textAlign:'left'}}>Hello there<h1 style={{textAlign:'left'}}>dd</h1>, my name is </h1>
+                                <h1 className="animatedText">Jonathan Lindqvist</h1>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <h4>I'm a {' '}
+                    <Typical
+                        loop={Infinity}
+                        wrapper="b"
+                        steps={[
+                            '23 years old ',
+                            'Developer ',
+                            5000,
+                            'Student ',
+                            5000,
+                            'Front end lover ',
+                            5000
+                        ]}
+                    />
+                    </h4>
+
+
+                    <br/>
+                    <br/>
+                    <br/>
                     <h3 className="text-dark">Go down and see all the projects</h3>
+
                 </div>
             </section>
 
             <section className="show bg-section text-white bg-dark">
-                <div className="sectionCenter">
+                <h1 className="fw-bold">
+                    Projects that I have built
+                    <MDBIcon icon="lightbulb " size="2px" className=""/>
+                </h1>
 
-                    <h1 className="fw-bold">
-                        Projects that I have built
-                        <MDBIcon icon="lightbulb " size="2px" className=""/>
-                    </h1>
+                <h2>Projects</h2>
+                <p>KTH Ergonomi</p>
 
-                    <h2>Projects</h2>
-                    <p>KTH Ergonomi</p>
-                </div>
 
             </section>
-            <section className="hidden">
+            <section className="show bg-section text-dark bg-white">
                 <h2>Projects</h2>
                 <p>Paint Master</p>
             </section>
