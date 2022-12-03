71a0b7704d67926ffc797d1fd493de8f42ce5f36
JLindqvistt <linkanjontes@gmail.com>
2022-12-03 14:32:17 +0100

    dedde

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,6 +4,24 @@ import {MDBIcon} from 'mdbreact';
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import Typical from 'react-typical'
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("activeSection");
+        } else {
+            reveals[i].classList.remove("activeSection");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
 const HomePage = () => {
     return (
         <div>
@@ -11,21 +29,34 @@ const HomePage = () => {
                 <div className="bg-image">
                     <div className="container">
                         <div className="row justify-content-md-center">
-                            <div className="col-sm-5">
-                                <h1 className="text-center" style={{textAlign: 'left'}}>Hello there, my name is </h1>
-                                <h1 className="animatedText">Jonathan Lindqvist.</h1>
-                                <h4 className="animatedLine">I'm a {' '}
+                            <div className="col-sm-8 text-center ">
+                                <div style={{
+                                    display: 'inline-block'
+                                }}>
+                                    <h1 style={{fontWeight:'bold'}} className="display-1 animatedLine" >Hello there,  {" "}</h1>
+                                </div>
+                                <div style={{
+                                    display: 'inline-block'
+                                }}>
+                                    <h1 className="display-1 text-white-50"> my name is </h1>
+                                </div>
+                                <div className="slideIn">
+                                    <h1 className="animatedLine ">Jonathan Lindqvist.</h1>
+                                </div>
+
+                                <h4 className="reveal">I'm a {' '}
                                     <Typical
+                                        className="animatedText"
                                         loop={Infinity}
                                         wrapper="b"
                                         steps={[
                                             '23 years old ',
                                             'Developer ',
-                                            5000,
+                                            2000,
                                             'Student ',
-                                            5000,
+                                            2000,
                                             'Frontend lover ',
-                                            5000
+                                            2000
                                         ]}
                                     />
                                 </h4>
@@ -36,25 +67,26 @@ const HomePage = () => {
                         </div>
                     </div>
                     <br/>
-
                 </div>
             </section>
             <section className="show bg-section text-dark" id={"aboutme"}>
-                <h2 style={{borderBottom: '2px solid #1f8696'}} className="text-white-50">About me</h2>
-                <div className="container-fluid">
-                    <div className="row justify-content-md-center" style={{paddingTop: '5vh'}}>
-                        <div className="col-lg-2" style={{textAlign: 'center'}}>
-                            <img src={require('../images/me.PNG')} alt='logo' width="150px"
-                                 style={{borderRadius: 100}}/>
-                        </div>
-                        <div className="col-lg-3">
-                            <p className="text-light">I am a young man who is 23 years old and was born and raised in
-                                Stockholm, Tyresö. Have worked a lot with the three architectures, frontend, backend and
-                                database. </p>
-                            <meter>ded</meter>
+
+                    <h2 style={{borderBottom: '2px solid #1f8696'}} className="text-white-50">About me</h2>
+                    <div className="container-fluid reveal">
+                        <div className="row justify-content-md-center" style={{paddingTop: '5vh'}}>
+                            <div className="col-lg-2" style={{textAlign: 'center'}}>
+                                <img src={require('../images/me.PNG')} alt='logo' width="150px"
+                                     style={{borderRadius: 100}}/>
+                            </div>
+                            <div className="col-lg-3 " >
+                                <p className="text-light">I am a young man who is 23 years old and was born and raised in
+                                    Stockholm, Tyresö. Have worked a lot with the three architectures, frontend, backend and
+                                    database. </p>
+                                <meter>ded</meter>
+                            </div>
                         </div>
                     </div>
-                </div>
+
 
 
             </section>
